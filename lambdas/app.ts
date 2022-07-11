fbd56c11dbd3cecb6539cdbed357ab1addc54578
Enrique M. Talavera <kiquetal@gmail.com>
2022-07-10 23:02:23 -0400

    testing using layers

--- lambdas/app.ts
+++ lambdas/app.ts
@@ -1,11 +1,12 @@
 import {v4 as uuid} from "uuid";
 import {format, parseISO} from 'date-fns'
-
+import * as AWS from "/opt/nodejs/aws-sdk"
 export const handler = async (event: any = {}, context: any = {}): Promise<any> => {
     let fakeResponse = {
         "items": ["uno", "dos", "tres"], "uuid": uuid(),
         "date": parseISO(new Date().toISOString())
     }
+
     try {
         return {statusCode: 200, body: JSON.stringify(fakeResponse)};
     } catch (dbError) {
